d8b1aec38fac9b6ab257bdc97906857e59aeebee
imaditya05 <imaditya05@gmail.com>
2024-08-12 14:40:57 +0530

    Added Toolbar Icons

--- app/board/[boardId]/_components/toolbar/page.tsx
+++ app/board/[boardId]/_components/toolbar/page.tsx
@@ -1,15 +1,69 @@
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
+import { ToolButton } from "../tool-button";
+
 const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className=" bg-white flex flex-col items-center gap-y-1 p-1.5 shadow-md rounded-md">
-        <div>Pencil</div>
-        <div>Square</div>
-        <div>Circle</div>
-        <div>Ellipsis</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Sticky Note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="flex flex-col items-center shadow-md rounded-md p-1.5 bg-white">
-        <div>Undo</div>
-        <div>Redo</div>
+      <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={false}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={false}
+        />
       </div>
     </div>
   );
@@ -17,7 +71,7 @@ const Toolbar = () => {
 
 export default Toolbar;
 
-export const ToolbarSkeleton = () =>  {
+export const ToolbarSkeleton = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
   );
