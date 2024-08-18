8e241b9ff1fb42011b8755dfa730795c8f6a7128
imaditya05 <imaditya05@gmail.com>
2024-08-18 13:02:04 +0530

    Added Selection box for self

--- app/board/[boardId]/_components/canvas/page.tsx
+++ app/board/[boardId]/_components/canvas/page.tsx
@@ -25,6 +25,7 @@ import { CursorsPresence } from "../cursors-presence";
 import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "../layer-preview";
+import { SelectionBox } from "../selection-box";
 
 const MAX_LAYERS = 100;
 
@@ -190,6 +191,9 @@ const Canvas = ({ boardId }: canvasProps) => {
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox 
+            onResizeHandlePointerDown={() => {}}
+          />
           <CursorsPresence />
         </g>
       </svg>
