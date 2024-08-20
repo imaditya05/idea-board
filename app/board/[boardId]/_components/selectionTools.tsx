6abfb92a363a4cf1b6765437cd1da6a1e367a11e
imaditya05 <imaditya05@gmail.com>
2024-08-20 14:28:37 +0530

    Added change layer colour

--- app/board/[boardId]/_components/selectionTools.tsx
+++ app/board/[boardId]/_components/selectionTools.tsx
@@ -1,16 +1,50 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { Camera, Color } from "@/types/canvas";
+import { useMutation, useSelf } from "@liveblocks/react/suspense";
 import { memo } from "react";
+import { ColorPicker } from "./color-picker";
 
-interface selectionToolsProps {
+interface SelectionToolsProps {
   camera: Camera;
   setLastUsedColor: (color: Color) => void;
 }
 
 export const SelectionTools = memo(
-  ({ camera, setLastUsedColor }: selectionToolsProps) => {
-    return <div>Selection Tools</div>;
+  ({ camera, setLastUsedColor }: SelectionToolsProps) => {
+    const selection = useSelf((me) => me.presence.selection);
+
+    const setFill = useMutation (
+      ({ storage }, fill: Color) => {
+        const liveLayers = storage.get("layers");
+        setLastUsedColor(fill);
+
+        selection.forEach((id) => {
+          liveLayers.get(id)?.set("fill", fill);
+        });
+      },
+      [selection, setLastUsedColor]
+    );
+
+    const selectionBounds = useSelectionBounds();
+
+    if (!selectionBounds) {
+      return null;
+    }
+
+    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+    const y = selectionBounds.y + camera.y;
+    return (
+      <div
+        className="absolute p-3 bg-white shadow-sm border flex select-none rounded-xl"
+        style={{
+          transform: `translate(calc(${x}px - 50%), calc(${y - 16}px - 100%)`,
+        }}
+      >
+        <ColorPicker onChange={setFill} />
+      </div>
+    );
   }
 );
 
